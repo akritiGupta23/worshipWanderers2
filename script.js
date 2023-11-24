56e87dff8ef32f359c04ceff8b4a1c9f0ea8b7a2
akritiGupta23 <129201893+akritiGupta23@users.noreply.github.com>
2023-11-24 16:54:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+$('#recipeCarousel').carousel({
+    interval: 3000
+    
+  })
+  
+  $('.carousel .carousel-item').each(function(){
+    var minPerSlide = 3;
+    var next = $(this).next();
+    if (!next.length) {
+    next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+    
+    for (var i=0;i<minPerSlide;i++) {
+        next=next.next();
+        if (!next.length) {
+            next = $(this).siblings(':first');
+          }
+        
+        next.children(':first-child').clone().appendTo($(this));
+      }
+});
+
+$('#recipe3Carousel').carousel({
+  interval: 1500
+  
+})
+
+$('.carousel .carousel-item').each(function(){
+  var minPerSlide = 3;
+  var next = $(this).next();
+  if (!next.length) {
+  next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+  
+  for (var i=0;i<minPerSlide;i++) {
+      next=next.next();
+      if (!next.length) {
+          next = $(this).siblings(':first');
+        }
+      
+      next.children(':first-child').clone().appendTo($(this));
+    }
+});
+
+  
